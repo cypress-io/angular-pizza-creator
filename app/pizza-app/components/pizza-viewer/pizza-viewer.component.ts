80d4d40f8ebe89ed720e506f8c0b28b0244d31da
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-05 17:17:41 -0500

    make pizza base green

--- app/pizza-app/components/pizza-viewer/pizza-viewer.component.ts
+++ app/pizza-app/components/pizza-viewer/pizza-viewer.component.ts
@@ -7,11 +7,11 @@ import { FormArray } from '@angular/forms';
 export const DROP_ANIMATION = trigger('drop', [
   transition(':enter', [
     style({ transform: 'translateY(-200px)', opacity: 0 }),
-    animate('5000ms cubic-bezier(1.000, 0.000, 0.000, 1.000)', style({ transform: 'translateY(0)', opacity: 1 }))
+    animate('300ms cubic-bezier(1.000, 0.000, 0.000, 1.000)', style({ transform: 'translateY(0)', opacity: 1 }))
   ]),
   transition(':leave', [
     style({ transform: 'translateY(0)', opacity: 1 }),
-    animate('2000ms cubic-bezier(1.000, 0.000, 0.000, 1.000)', style({ transform: 'translateY(-200px)', opacity: 0 }))
+    animate('200ms cubic-bezier(1.000, 0.000, 0.000, 1.000)', style({ transform: 'translateY(-200px)', opacity: 0 }))
   ])
 ]);
 
